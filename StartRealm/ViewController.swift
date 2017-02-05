9d0408a62af4e8ad94bfa4938620a1abac9b7e7d
Eunjoo <eunjoo.im@realm.io>
2017-02-05 16:33:28 +0900

    Add first data to Realm

--- StartRealm/ViewController.swift
+++ StartRealm/ViewController.swift
@@ -13,8 +13,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addDiary()
     }
-
+    
+    func addDiary() {
+        // 기본 Realm을 가져옵니다.
+        let realm = try! Realm()
+        
+        // Picture 객체를 만들고 값을 넣습니다.
+        let testPicture1 = Picture(value:["01.jpg"])
+        let testPicture2 = Picture(value:["02.jpg"])
+        
+        // Diary 객체를 만들고 값을 넣습니다.
+        let myDiary = Diary(value: [incrementID(), Date(), "안녕하세요?", [testPicture1, testPicture2]]);
+        
+        // 트랜잭션 안에서 Realm에 Diary 객체를 추가합니다.
+        try! realm.write {
+            realm.add(myDiary)
+        }
+        
+        // Realm 경로를 찾습니다.
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+    }
+    
+    // ID 값을 증가시킵니다.
+    func incrementID() -> Int {
+        let realm = try! Realm()
+        return (realm.objects(Diary.self).max(ofProperty: "id") as Int? ?? 0) + 1
+    }
 }
 
